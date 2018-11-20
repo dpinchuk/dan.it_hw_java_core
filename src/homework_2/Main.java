163d4291d473f8d55ea92c22ee2c96bc9be8487b
dpinchuk <pinchuk.dap@gmail.com>
2018-11-20 22:24:36 +0200

    in progress...

--- src/homework_2/Main.java
+++ src/homework_2/Main.java
@@ -14,64 +14,86 @@ public class Main {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+
         final int M = 5;
         final int N = 5;
-        int targetCapacity = 3;
-        int[][] square = new int[M][N];
+        int targetCapacity = 3; // Only less M or N !!!
 
         System.out.println("All set. Get ready to rumble!\n");
 
-        // init target coordinates
-        int xTarget = random.nextInt(M);
-        int yTarget = random.nextInt(N);
-        square[xTarget][yTarget] = 2;
+        int xTarget = new Random().nextInt(M - 1);
+        int yTarget = new Random().nextInt(N - 1);
+
+        int[][] square = setTarget(targetCapacity, xTarget, yTarget, M, N);
+        printBattleArray(square, M, N, false);
 
         // Endless loop
         while (true) {
-            System.out.print("Enter the Row for shooting: ");
+            System.out.print("\nEnter the Row for shooting [1, 5]: ");
             int shootingRow = scanner.nextInt();
             // Checking of the entered number
-            while (shootingRow < 1 & shootingRow > N) {
-                System.out.print("Enter the correct line for shooting: ");
+            while (shootingRow < 1 || shootingRow > M) {
+                System.out.print("Enter the correct Row for shooting: ");
                 shootingRow = scanner.nextInt();
             }
+            shootingRow--;
 
-            System.out.print("Enter the Column for shooting: ");
+            System.out.print("\nEnter the Column for shooting [1, 5]: ");
             // Checking of the entered number
             int shootingColumn = scanner.nextInt();
-            while (shootingColumn < 1 & shootingColumn > N) {
-                System.out.print("Enter the correct line for shooting: ");
-                shootingRow = scanner.nextInt();
+            while (shootingColumn < 1 || shootingColumn > N) {
+                System.out.print("Enter the correct Column for shooting: ");
+                shootingColumn = scanner.nextInt();
             }
+            shootingColumn--;
 
-            if (square[shootingRow][shootingColumn] == 2) {
-                System.out.println("You have won!");
-                break;
+            if (square[shootingRow][shootingColumn] == 1) {
+                square[shootingRow][shootingColumn] = 2;
             } else {
-                // The array element is marked 1
-                square[shootingRow][shootingColumn] = 1;
+
             }
 
+            break;
         }
-
     }
 
-    private int[][] setTarget(int[][] arr, int targetCapacity, int xTarget, int yTarget) {
-        if (xTarget == 0) {
+    private static int[][] setTarget(int targetCapacity, int shootingRow, int shootingColumn, int m, int n) {
+        //vertical = true; Horizontal = false
+        int[][] battleField = new int[m][n];
+        boolean isVertical = new Random().nextBoolean();
+        int index = 0;
+        if (isVertical) {
+            for (int j = 0; j < targetCapacity; j++) {
+                if (shootingColumn + j > n - 1) {
+                    battleField[shootingRow][index++] = 1;
+                } else {
+                    battleField[shootingRow][shootingColumn + j] = 1;
+                }
+            }
+        } else {
             for (int i = 0; i < targetCapacity; i++) {
-                arr[i][yTarget] = 2;
+                if (shootingRow + i > m - 1) {
+                    battleField[index++][shootingColumn] = 1;
+                } else {
+                    battleField[shootingRow + i][shootingColumn] = 1;
+                }
             }
-        } else if (xTarget == arr.length) {
-
         }
-
-        return null;
+        return battleField;
     }
 
-    private void printSquare(int[][] arr, int m, int n) {
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                System.out.println();
+    private static void printBattleArray(int[][] arr, int m, int n, boolean isHide) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isHide) {
+                    if (arr[i][j] == 1) {
+                        System.out.print(0 + " ");
+                    } else {
+                        System.out.print(arr[i][j] + " ");
+                    }
+                } else {
+                    System.out.print(arr[i][j] + " ");
+                }
             }
             System.out.println();
         }
