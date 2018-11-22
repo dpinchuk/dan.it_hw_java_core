a2a4a98a78d36ae0fea321b4b84f5a0ec89ad8a9
dpinchuk <pinchuk.dap@gmail.com>
2018-11-22 10:48:46 +0200

    added homework_3

--- src/homework_3/Main.java
+++ src/homework_3/Main.java
@@ -38,49 +38,70 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String inputDay = "";
         boolean isExit = false;
-        String[] arrStr;
 
-        while(true) {
+        while (true) {
             System.out.print("Please, input the day of the week: ");
             inputDay = scanner.nextLine();
             inputDay = inputDay.toLowerCase().trim();
-            arrStr = inputDay.split(" ");
-            if (arrStr.length == 2 && )
 
-            switch (inputDay) {
-                case "sunday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[0][1] + "'");
-                    break;
-                case "monday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[1][1] + "'");
-                    break;
-                case "tuesday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[2][1] + "'");
-                    break;
-                case "wednesday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[3][1] + "'");
-                    break;
-                case "thursday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[4][1] + "'");
-                    break;
-                case "friday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[5][1] + "'");
-                    break;
-                case "saturday":
-                    System.out.println("\nYour tasks for " + inputDay + ": '" + scedule[6][1] + "'");
-                    break;
-                case "exit":
-                    System.out.println("\nGoodbye!");
-                    isExit = true;
-                    break;
-                default:
-                    System.out.println("\nSorry, I don't understand you, please try again");
-            }
-            if (isExit) {
+            if (inputDay.equals("exit")) {
                 break;
             }
 
+            if (inputDay.split(" ").length == 1) {
+                for (int i = 0; i < scedule.length; i++) {
+                    if (inputDay.equals(scedule[i][0])) {
+                        getScheduleForCurrentDay(scedule[i][0], scedule);
+                        break;
+                    }
+                    if (i == scedule.length - 1) {
+                        System.out.println("Sorry, I don't understand you, please try again");
+                        break;
+                    }
+                }
+            } else if (inputDay.split(" ").length == 2) {
+                for (int i = 0; i < scedule.length; i++) {
+                    if ((inputDay.contains(scedule[i][0])) &&
+                            (inputDay.contains("change") || inputDay.contains("reschedule"))) {
+                        setScheduleForCurrentDay(scedule[i][0], scedule, i, scanner);
+                        break;
+                    }
+                    if (i == scedule.length - 1) {
+                        System.out.println("Sorry, I don't understand you, please try again");
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Sorry, I don't understand you, please try again");
+            }
+
         }
+    }
+
+    private static void setScheduleForCurrentDay(String schedule, String[][] scedule, int i, Scanner scanner) {
+        System.out.println("\nPlease, input new tasks for " + schedule);
+        scedule[i][1] = scanner.nextLine();
+
+    }
+
+    private static String getScheduleForCurrentDay(String day, String[][] scedule) {
+        switch (day) {
+            case "sunday":
+                return "\nYour tasks for " + day + ": '" + scedule[0][1] + "'";
+            case "monday":
+                return "\nYour tasks for " + day + ": '" + scedule[1][1] + "'";
+            case "tuesday":
+                return "\nYour tasks for " + day + ": '" + scedule[2][1] + "'";
+            case "wednesday":
+                return "\nYour tasks for " + day + ": '" + scedule[3][1] + "'";
+            case "thursday":
+                return "\nYour tasks for " + day + ": '" + scedule[4][1] + "'";
+            case "friday":
+                return "\nYour tasks for " + day + ": '" + scedule[5][1] + "'";
+            case "saturday":
+                return "\nYour tasks for " + day + ": '" + scedule[6][1] + "'";
+        }
+        return "\nSorry, I don't understand you, please try again";
     }
 
 }
